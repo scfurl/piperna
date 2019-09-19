e12c2acdf1f9d290f9e9f482894f86469a431e6c
scfurl <sfurlan@uw.edu>
2019-09-18 17:12:21 -0700

    nm

--- piperna/piperna.py
+++ piperna/piperna.py
@@ -34,6 +34,7 @@ import random
 from itertools import chain, compress
 import json
 
+_ROOT = os.path.abspath(os.path.dirname(__file__))
 GENOMES_JSON = os.path.join(_ROOT, 'data', 'genomes.json')
 
 class SampleFactory:
