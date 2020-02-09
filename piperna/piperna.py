d01d5353fc9ed6575f6b057811fb33558873952b
scfurl <sfurlan@uw.edu>
2020-02-08 21:19:24 -0800

    cleaner

--- piperna/piperna.py
+++ piperna/piperna.py
@@ -16,83 +16,127 @@ _ROOT = os.path.abspath(os.path.dirname(__file__))
 #GENOMES_JSON = os.path.join('/Users/sfurla/Box Sync/PI_FurlanS/computation/develop/piperna/piperna/data/genomes.json')
 GENOMES_JSON = os.path.join(_ROOT, 'data', 'genomes.json')
 SUMMARIZE_SCRIPT = os.path.join(_ROOT, 'scripts', 'summarize.R')
+ENVIRONS_JSON = os.path.join(_ROOT, 'data', 'environs.json')
 
 class SampleFactory:
-    def __init__(self, *args, **kwargs):
+        self.environs = environs(cluster = kwargs.get('cluster'), user = kwargs.get('user'), log = kwargs.get('log'), threads = kwargs.get('threads'), gb_ram = kwargs.get('gb_ram'))
+
+        #remove later
         self.user = kwargs.get('user')
         self.cluster = kwargs.get('cluster')
+        self.threads = kwargs.get('threads')
+        self.gb_ram = kwargs.get('gb_ram')
+        self.cluster = kwargs.get('cluster')
+
         self.runsheet_data = kwargs.get('runsheet_data')
         self.debug = kwargs.get('debug')
-        self.log_name = kwargs.get('log')
+        # self.log_name = kwargs.get('log')
     def __call__():
         pass
-        # if self.debug == False:
-        #   open(self.log, 'w')
 
-    def id_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
+    def id_generator(self, size=10, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
 
-    def get_runmode(self):
-        mode = ""
-        if "fastq1" in self.runsheet_data[0]:
-            if "fastq2" in self.runsheet_data[0]:
-                mode = "pe"
-        if "fastqs" in self.runsheet_data[0]:
-            mode = "single"
-        return mode
-    def generate_job(self):
-        job_string=[]
-        torun = len(self.runsheet_data)
-        for i in range(torun):
-            log_file = self.id_generator()
-            job_name = self.job + "_" + self.runsheet_data[i]['sample']
-            command = self.command[i]
-            if self.cluster=="PBS":
-                to_append = "#!/bin/bash\n#PBS -N %s\n#PBS -l %s\n#PBS -j oe\n#PBS -o $PBS_O_WORKDIR/logtmp\n#PBS -A %s\ncd $PBS_O_WORKDIR\n{%s} 2>&1 | tee %s\nsed -e 's/^/[piperna] JOB: %s:\t\t/' %s >> %s\nrm %s\n" % (job_name, self.processor_line, self.user, command, log_file, job_name, log_file, self.log_name, log_file)
-            if self.cluster=="SLURM":
-                to_append = '#!/bin/bash\n#SBATCH --job-name=%s\n#SBATCH --ntasks=1\n\n%s' % (job_name, self.processor_line, command)
-            job_string.append(to_append)
-        return job_string
-
-    def get_processor_line(self):
-        if self.cluster=="PBS":
-            gb = self.threads * 8
-            return """select=1:mem=%sGB:ncpus=%s""" % (gb, self.threads)
-        if self.cluster=="SLURM":
-            gb = self.threads * 8000
-            return """SBATCH --cpus-per-task=%s\n#SBATCH --mem-per-cpu=%s"""% (self.threads, gb)
-
-
     def run_job(self):
-        popen_commands = {"PBS":'qsub', "SLURM":['sbatch']}
-        popen_command = popen_commands.get(self.cluster)
-        for script in self.script:
-            if self.cluster=="PBS":
-                if self.debug==False:
-                    # Open a pipe to the command.
-                    proc = Popen(popen_command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
-                    if (sys.version_info > (3, 0)):
-                        proc.stdin.write(script.encode('utf-8'))
-                        out, err = proc.communicate()
-                    else:
-                        proc.stdin.write(script)
-                        out, err = proc.communicate()
-                # Print your job and the system response to the screen as it's submitted
-                print(script)
-                if self.debug==False:
-                    print(out)
-                    time.sleep(0.1)
+        popen_command = self.environs.popen_command
+        for script in self.bash_scripts:
+            if self.debug==False:
+                # Open a pipe to the command.
+                proc = Popen(popen_command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+                if (sys.version_info > (3, 0)):
+                    proc.stdin.write(script.encode('utf-8'))
+                    out, err = proc.communicate()
+                else:
+                    proc.stdin.write(script)
+                    out, err = proc.communicate()
+            # Print your job and the system response to the screen as it's submitted
+            print(script)
+            if self.debug==False:
+                print(out)
+                time.sleep(0.1)
+
+
+
+
+class environs:
+    def __init__(self, *args, **kwargs):
+        self.cluster = kwargs.get('cluster')
+        self.user = kwargs.get('user')
+        self.log = kwargs.get('log')
+        self.environs_data = self.load_environs(ENVIRONS_JSON).get(self.cluster)
+        self.popen_command = self.environs_data["popen"]
+        self.threads = kwargs.get('threads')
+        self.ram = kwargs.get('gb_ram')
+
+    def id_generator(self, size=10, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+    def load_environs(self, environs_file):
+        with open(environs_file, "r") as read_file:
+            data = json.load(read_file)
+        return data
+
+    def get_processor_line(self, *args, **kwargs):
+        if kwargs.get('threads') is None:
+            threads = self.environs_data["resources"][kwargs.get('job')]["threads"]
+        else:
+            threads = kwargs.get('threads')
+        if kwargs.get('gb_ram') is None:
+            ram = self.environs_data["resources"][kwargs.get('job')]["ram"]
+        else:
+            ram = kwargs.get('ram')
+        return [threads, ram]
+
+    def generate_job(self, commands, job):
+        bash_scripts=[]
+        torun = len(commands)
+        threads,ram = self.get_processor_line(threads = self.threads, ram = self.ram, job = job)
+        for i in range(torun):
+            # bash_script = {  "PBS" : "#!/bin/bash\n#PBS -N %s\n#PBS -l %s\n#PBS -j oe\n#PBS -o $PBS_O_WORKDIR/logtmp\n#PBS -A %s\ncd $PBS_O_WORKDIR\n{%s} 2>&1 | tee %s\nsed -e 's/^/[HENIPIPE] JOB: %s:\t\t/' %s >> %s\nrm %s\n" % (job_name, self.processor_line, self.user, command, log_file, job_name, log_file, self.log_name, log_file),
+                            # "SLURM" : "#!/bin/bash\n#SBATCH --job-name=%s\n#SBATCH --output=tmp\n#SBATCH --error=tmp\n#SBATCH --ntasks=1\n%s\n{%s} 2>&1 | tee %s\nsed -e 's/^/[HENIPIPE] JOB: %s:\t\t/' %s >> %s\nrm %s\n" % (job_name, self.processor_line, command, log_file, job_name, log_file, self.log_name, log_file)}
+            bash_script = self.assemble_script(   LOG_FILE = self.log, \
+                                    MODULES = self.environs_data["resources"][job]["modules"], \
+                                    TEMP_LOG_FILE = self.id_generator(), \
+                                    JOB_NAME = (job + "_" + commands[i][0]), \
+                                    COMMAND = commands[i][1],
+                                    RAM = ram,
+                                    THREADS = threads,
+                                    USER = self.user)
+            bash_scripts.append(bash_script)
+        return bash_scripts
+
+    def assemble_script(self, *args, **kwargs):
+        script_list=[]
+        order=[]
+        fn_args = kwargs
+        for key, value in self.environs_data["script_lines"].items():
+            script_list.append(value)
+            order.append(key)
+        script_list = [x for _,x in sorted(zip(order,script_list))]
+        lines_unparsed = [x[0] for x in script_list]
+        values_to_insert = [x[1].split("|") for x in script_list]
+        lines_parsed = []
+        global to_test
+        to_test=[lines_unparsed, values_to_insert, fn_args]
+        for i in range(len(lines_unparsed)):
+            if values_to_insert[i][0] is "":
+                lines_parsed.append(lines_unparsed[i])
+            else:
+                string=lines_unparsed[i]
+                for j in range(len(values_to_insert[i])):
+                    string = re.sub("<--{0}-->".format(j), fn_args.get(values_to_insert[i][j]), string)
+                lines_parsed.append(string)
+        return "\n".join(lines_parsed)
+
 
 class Star(SampleFactory, object):
     def __init__(self, *args, **kwargs):
         super(Star, self).__init__(*args, **kwargs)
-        self.threads = kwargs.get('threads')
         self.job = "STAR_ALIGN"
         self.out_sam_type = kwargs.get('out_sam_type')
         self.count = kwargs.get('count')
         self.global_add_STAR_string = kwargs.get('global_add_STAR_string')
         self.runmode = self.get_runmode()
-        self.processor_line = self.get_processor_line()
         self.command = self.Star_executable()
         self.script = self.generate_job()
     def __call__():
