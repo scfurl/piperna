b35eba2d334ee179f640cd1ad1786a5cc7b31049
scfurl <sfurlan@uw.edu>
2020-03-20 19:51:17 -0700

    vb

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
     setuptools.setup(
     name="piperna",
-    version="1.4",
+    version="1.41",
     author="Scott Furlan",
     author_email="scottfurlan@gmail.com",
     description="A python wrapper for fast and parallel processing of bulk RNA Seq data",
